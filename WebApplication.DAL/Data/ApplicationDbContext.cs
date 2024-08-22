f93151e682d72c22aff34dfc7b97c61ec05ab1be
Basel638 <basselsameh2004@gmail.com>
2024-08-22 21:24:12 +0300

    Add project files.

---
+++ WebApplication.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.DAL.Data.Configurations;
+using WebApplication.DAL.Models;
+
+namespace WebApplication.DAL.Data
+{
+	internal class ApplicationDbContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+			=> optionsBuilder.UseSqlServer("Server = .; Database = MVCApplication; Trusted_Connection = True;");
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			//modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
+		public DbSet<Department> Departments { get; set; }
+    }
+}
